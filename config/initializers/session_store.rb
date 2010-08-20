6622dec9e5ebb6ebafdee96b90c1cdcb72beadae
Carl Tanner <carl@captproton.com>
2010-08-19 19:09:10 -0700

    Initial commit

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,8 +5,8 @@
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
-  :key    => "_CHANGEME_session",
-  :secret => "CHANGESESSION"
+  :key    => "_meetup_getup_session",
+  :secret => "9ae160b50af10e0048933e8ed00342ef"
 }
 
 # Use the database for sessions instead of the cookie-based default,
