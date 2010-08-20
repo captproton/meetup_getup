f43c4b5a9678feed8594aac5acf4e9bca981247b
Carl Tanner <carl@captproton.com>
2010-08-19 19:09:42 -0700

    installed clearance

--- features/step_definitions/factory_girl_steps.rb
+++ features/step_definitions/factory_girl_steps.rb
@@ -1,1 +1,5 @@
-require 'factory_girl/step_definitions'
+Factory.factories.each do |name, factory|
+  Given /^an? #{name} exists with an? (.*) of "([^"]*)"$/ do |attr, value|
+    Factory(name, attr.gsub(' ', '_') => value)
+  end
+end
